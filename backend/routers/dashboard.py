9ff88d20b64d855d97d9beb230c3f77b063cd80b
Timka95 <timeakata2001@gmail.com>
2026-02-27 23:14:00 +0100

    foods sorry this one

--- backend/routers/dashboard.py
+++ backend/routers/dashboard.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
 from dbCalls.usersCalls import UserSetter
 from dbCalls.usersCalls import UserCretor_Class
+from dbCalls.fooddata import ManageFood_Class
 
 # call it by prefix when fetching
 router = APIRouter(prefix="/dashboard", tags=["dashboard"])
@@ -11,3 +12,9 @@ router = APIRouter(prefix="/dashboard", tags=["dashboard"])
 def get_first_name(user_id: int):
     name = UserCretor_Class.get_name_by_id(user_id)
     return {"status": "success", "name": name}
+
+
+@router.post("/login/{user_id}")
+def get_food_items(user_id: int):
+    items = ManageFood_Class.list_items_userid(user_id)
+    return items
