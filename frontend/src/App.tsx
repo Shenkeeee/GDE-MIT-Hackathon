52ba4bfe6d02daa6d65addb95d23dbd2a080a21f
Shenkeeee <mate.subicz@gmail.com>
2026-02-27 17:49:41 +0100

    Make home and register and login nicer, add framer motion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
 import DashboardPage from "./components/DashboardPage/DashboardPage";
-import Register from "./components/Register/Register";
 
 const App = () => {
   return (
     <div className="w-screen h-screen flex flex-col gap-2 justify-center items-center">
-      <Header />
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </BrowserRouter>
-
-      <Footer />
     </div>
   );
 };
