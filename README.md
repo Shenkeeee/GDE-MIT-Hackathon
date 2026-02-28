a41c2605d4cbc2a7fc1a24db85e78381abd48212
Shenkeeee <mate.subicz@gmail.com>
2026-02-28 02:34:04 +0100

    Ensure deploy, add prod things

--- README.md
+++ README.md
@@ -22,6 +22,14 @@ Timea Subicz, Mate Subicz, Benjamin Tako
   - check if you have done it correctly: bash style: `which python`, powershell style: `Get-Command python` => You should see that python is ran from .venv folder
   - run `uvicorn main:app --reload`
 
+## Deploy - prod
+
+1. Change .env in frontend to PROD
+1. Change main.py to PROD
+1. build frontend with `pnpm run build`
+1. run backend with `uvicorn main:app --host 0.0.0.0 --port 8000`
+1. deploy with `ngrok http 8000`
+
 ### Auto-terminal initialization
 
 After you have succesfully ran the app manually and have all the stuff required, download the **Terminal Keeper** extension that uses `sessions.json`, it will load up all the required terminals for you automatically and start both the frontend and backend when you open the project.
--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routers import items, users, dashboard , testalldataaiandmore
 
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+
+### DEV ###
+
 app = FastAPI()
 
 # CORS setup - allow client to request from port
@@ -23,3 +30,25 @@ app.add_middleware(
 app.include_router(items.router)
 app.include_router(users.router)
 app.include_router(dashboard.router)
+
+
+# ### PROD ###
+
+# app = FastAPI()
+
+# # Include routes
+# app.include_router(items.router)
+# app.include_router(users.router)
+# app.include_router(dashboard.router)
+
+# # Absolute path (safer on Windows)
+# BASE_DIR = Path(__file__).resolve().parent
+# DIST_DIR = BASE_DIR.parent / "frontend" / "dist"
+
+# app.mount("/", StaticFiles(directory=DIST_DIR, html=True), name="static")
+
+
+# # Serve React app
+# @app.get("/{full_path:path}")
+# async def serve_react(full_path: str):
+#     return FileResponse(DIST_DIR / "index.html")
